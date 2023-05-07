5b342ce1cd3e76e2b65ca67b1d13dd48ec9ef232
owner <37741728+igz0@users.noreply.github.com>
2023-05-07 20:49:21 +0900

    add: 通知画面にいいね・リポスト・リプライされた投稿文を表示

--- lib/view/notifications.dart
+++ lib/view/notifications.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:bluesky/bluesky.dart' as bsky;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:math';
+import 'package:skyclad/view/post_details.dart';
 
 import 'package:skyclad/providers/providers.dart';
 import 'package:skyclad/view/user_profile.dart';
@@ -8,26 +10,52 @@ import 'package:skyclad/view/user_profile.dart';
 class NotificationScreen extends ConsumerWidget {
   const NotificationScreen({Key? key}) : super(key: key);
 
-  // 通知を取得する
-  Future<List<dynamic>> fetchNotifications(bsky.Bluesky bluesky) async {
-    final feeds = await bluesky.notifications.findNotifications();
-    return feeds.toJson()['notifications'];
-  }
+  String _getNotificationTitle(Map<String, dynamic> notification) {
+    var author = notification['author'];
+    String authorDisplayName = author['displayName'] ?? '';
 
-  // 通知の種類に応じて通知の内容を返す
-  String _getNotificationText(Map<String, dynamic> notification) {
     switch (notification['reason']) {
       case 'follow':
-        return 'フォローされました。';
+        return '$authorDisplayName がフォローしました。';
       case 'like':
-        return 'いいね！されました。';
+        return '$authorDisplayName がいいね！しました。';
       case 'repost':
-        return 'リポストされました。';
+        return '$authorDisplayName がリポストしました。';
       case 'reply':
-        return 'リプライされました。';
+        return '$authorDisplayName がリプライしました。';
       default:
         return '不明な通知です。';
     }
+  }
+
+  String _getPostText(Map<String, dynamic> post) {
+    final record = post['record'] ?? {};
+    return record?['text'] ?? '';
+  }
+
+  // 通知と投稿を取得する
+  Future<Map<String, List<dynamic>>> fetchNotificationsAndPosts(
+      bsky.Bluesky bluesky) async {
+    final notifications = await bluesky.notifications.findNotifications();
+    final List<dynamic> notificationsJson =
+        notifications.toJson()['notifications'] as List<dynamic>;
+    final List<String> uris = notificationsJson
+        .map((notification) => notification['reasonSubject'])
+        .where((uri) => uri != null)
+        .cast<String>()
+        .toList();
+
+    List<dynamic> postsJson = [];
+    int batchSize = 25;
+    for (int i = 0; i < uris.length; i += batchSize) {
+      final batchUris = uris.sublist(i, min(i + batchSize, uris.length));
+      final posts = await bluesky.feeds.findPosts(
+        uris: batchUris.map((uri) => bsky.AtUri.parse(uri)).toList(),
+      );
+      postsJson.addAll(posts.toJson()['posts'] as List<dynamic>);
+    }
+
+    return {'notifications': notificationsJson, 'posts': postsJson};
   }
 
   @override
@@ -37,43 +65,79 @@ class NotificationScreen extends ConsumerWidget {
     return Scaffold(
       body: blueskyAsyncValue.when(
         data: (bsky.Bluesky bluesky) {
-          return FutureBuilder<List<dynamic>>(
-            future: fetchNotifications(bluesky),
+          return FutureBuilder<Map<String, List<dynamic>>>(
+            future: fetchNotificationsAndPosts(bluesky),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const Center(child: CircularProgressIndicator());
               } else if (snapshot.hasError) {
                 return Center(child: Text('エラー: ${snapshot.error}'));
               } else {
-                List<dynamic> notifications = snapshot.data!;
+                List<dynamic> notifications = [];
+                List<dynamic> posts = [];
+
+                if (snapshot.hasData) {
+                  notifications =
+                      (snapshot.data!['notifications'] as List<dynamic>)
+                          .map((e) => e as Map<String, dynamic>)
+                          .toList();
+                  posts = (snapshot.data!['posts'] as List<dynamic>)
+                      .map((e) => e as Map<String, dynamic>)
+                      .toList();
+                }
+
                 return ListView.builder(
                   itemCount: notifications.length,
                   itemBuilder: (context, index) {
                     var notification = notifications[index];
+                    var post = posts.firstWhere(
+                      (post) => post['uri'] == notification['reasonSubject'],
+                      orElse: () => <String, dynamic>{},
+                    );
+
                     var author = notification['author'];
-                    return ListTile(
-                      leading: GestureDetector(
-                        onTap: () {
-                          // ユーザー詳細画面への遷移
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => UserProfileScreen(
-                                actor: author['handle'],
-                              ),
+                    return Column(
+                      children: [
+                        ListTile(
+                          leading: GestureDetector(
+                            onTap: () {
+                              // ユーザー詳細画面への遷移
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => UserProfileScreen(
+                                    actor: author['handle'],
+                                  ),
+                                ),
+                              );
+                            },
+                            child: CircleAvatar(
+                              backgroundImage:
+                                  NetworkImage(author['avatar'] ?? ''),
                             ),
-                          );
-                        },
-                        child: CircleAvatar(
-                          backgroundImage: NetworkImage(author['avatar'] ?? ''),
+                          ),
+                          title: Text(_getNotificationTitle(notification)),
+                          subtitle: Text(_getPostText(post)),
+                          trailing: !notification['isRead']
+                              ? const Icon(Icons.fiber_manual_record,
+                                  color: Colors.blue)
+                              : null,
+                          onTap: () {
+                            // 投稿詳細画面への遷移
+                            if (post.isNotEmpty) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => PostDetails(
+                                    post: post,
+                                  ),
+                                ),
+                              );
+                            }
+                          },
                         ),
-                      ),
-                      title: Text(author['displayName'] ?? ''),
-                      subtitle: Text(_getNotificationText(notification)),
-                      trailing: !notification['isRead']
-                          ? const Icon(Icons.fiber_manual_record,
-                              color: Colors.blue)
-                          : null,
+                        const Divider(color: Colors.grey),
+                      ],
                     );
                   },
                 );
